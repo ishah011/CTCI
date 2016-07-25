9fe8eaca368b6440dbb4e252d0d99c31d709a8e7
Isheeta Shah <isheeta_s@yahoo.com>
2016-07-24 21:49:18 -0500

    Added 1.5
    Write a method to replace all spaces in a string with "%20"

---
+++ 1point5.java
@@ -0,0 +1,18 @@
+// Write a method to replace all spaces in a string with a "%20"
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution
+{
+	public static void replaceString(String input)
+	{
+		String edited = input.replaceAll(" ","%20");
+	}
+	public static void main(String[] args)
+	{
+		input = "Hi there"
+		replaceString(input);
+	}
+}
