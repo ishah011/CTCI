f98849f88f3cc5f11abf7cf3f6d7909a3359509a
Isheeta Shah <isheeta_s@yahoo.com>
2016-07-28 16:37:38 -0500

    updated with 2.5 comments

--- 2point5.java
+++ 2point5.java
@@ -4,3 +4,6 @@
 ** The digits are stored in reverse order, such that the Ts digit is at the head of the list. 
 ** Write a function that adds the two numbers and returns the sum as a linked list.
 */
+
+//traverse both lists at the same time
+//take the data of the first node of list 1, add it to the data of the first node of list 2, and store that number in the first node of the sum list
