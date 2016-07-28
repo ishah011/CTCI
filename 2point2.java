418be21620322327abcabce93a324eeef42b01c8
Isheeta Shah <isheeta_s@yahoo.com>
2016-07-28 13:07:07 -0500

    Added class definition

--- 2point2.java
+++ 2point2.java
@@ -1,42 +1,45 @@
 import java.util.*
 
-public static int getLength(Node head)
+public class ListRemoval
 {
-  Node cur = head;
-  int length = 0;
-  while(curr != null)
+  public static int getLength(Node head)
   {
-    length++;
-    cur = curr.next;
-  }
-  return length;
-}
-
-public static void removeNth(Node head, int n, int length)
-{
-  int nthToLast = length - n;
-  if(nthToLast < 0)
-  {
-    System.out.println("Invalid removal value");
-    return;
+    Node cur = head;
+    int length = 0;
+    while(curr != null)
+    {
+      length++;
+      cur = curr.next;
+    }
+    return length;
   }
   
-  int iterator = 0;
-  Node curr = head;
-  while(iterator != nthToLast - 1)
+  public static void removeNth(Node head, int n, int length)
   {
-    curr = curr.next;
-    iterator++;
+    int nthToLast = length - n;
+    if(nthToLast < 0)
+    {
+      System.out.println("Invalid removal value");
+      return;
+    }
+    
+    int iterator = 0;
+    Node curr = head;
+    while(iterator != nthToLast - 1)
+    {
+      curr = curr.next;
+      iterator++;
+    }
+    curr.next = curr.next.next;
   }
-  curr.next = curr.next.next;
-}
-
-public static void main (String[] args)
-{
-  List list = new List();
-  list.head = addElements(list); //arbitrary function that adds to the list and returns the head
-  int sizeOf;
-  sizeOf = getLength(list.head);
-  int n = 3;
-  removeNth(list.head, n, sizeOf);
+  
+  public static void main (String[] args)
+  {
+    List list = new List();
+    list.head = addElements(list); //arbitrary function that adds to the list and returns the head
+    int sizeOf;
+    sizeOf = getLength(list.head);
+    int n = 3;
+    removeNth(list.head, n, sizeOf);
+  }
 }
