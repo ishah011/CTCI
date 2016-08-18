1fb642abc1490456b573c78214fe611bc01a17f0
Isheeta Shah <isheeta_s@yahoo.com>
2016-08-18 17:59:02 -0500

    fixed 4.5 java version

--- 4point5.java
+++ 4point5.java
@@ -1,15 +1,22 @@
-boolean isBST(TreeNode root)
+/*
+* 4.5
+* Implement a function to check if a binary tree is a binary search tree
+*
+*/
+
+boolean isBST(TreeNode root, boolean answer)
 {
-	if(!subroot.left && !subroot.right)
+	if((subroot.left && subroot.left.data > subroot.data) || (subroot.right && subroot.right.data < subroot.data))
 	{
-		return true;
+		answer = false
+		return answer;
 	}
-	else if(subroot.left > subroot || subroot.right < subroot)
-	{
-		return false;
-	}
+	else if(!subroot.left && !subroot.right)
+		return answer;
+	else if(!subroot.left)
+		return answer;
+	else if(!subroot.right)
+		return answer;
 	else
-	{
-		 return (isBST(subroot.left) && isBST(subroot.right));
-	}
+		return (isBST(subroot.left, answer) && isBST(subroot.right, answer));
 }
